006660ba392cdd9f0ea290834b9c165dcf47f6f0
francoperez03 <franco.perez03@gmail.com>
2025-02-10 10:37:14 -0300

    feat: created new endpoint to approve proof generation

--- packages/api/src/routes/proof/route.ts
+++ packages/api/src/routes/proof/route.ts
@@ -1,13 +1,25 @@
+import { CIRCUIT_TYPE } from "@/constants.js";
 import { GenerateProofDto } from "@/dtos/generate-proof.dto.js";
 import { generateProof } from "@/services/proof.service.js";
 import { Request, Response, Router, NextFunction } from "express";
 
 const router = Router();
 
-router.post("/generate", async (req: Request, res: Response, next: NextFunction) => {
+router.post("/transfer", async (req: Request, res: Response, next: NextFunction) => {
   try {
     const body: GenerateProofDto = req.body;
-    const proofArray = await generateProof(body);
+    const proofArray = await generateProof(CIRCUIT_TYPE.TRANSFER, body);
+
+    res.status(201).send(proofArray);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/approve", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const body: GenerateProofDto = req.body;
+    const proofArray = await generateProof(CIRCUIT_TYPE.APPROVE, body);
 
     res.status(201).send(proofArray);
   } catch (error) {
