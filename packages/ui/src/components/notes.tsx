1199da55f4a56ec52144238cf034617bd30f2b69
Matias Zapata <matuzapata@gmail.com>
2025-02-12 12:02:57 -0300

    connect notes

--- packages/ui/src/components/notes.tsx
+++ packages/ui/src/components/notes.tsx
@@ -1,12 +1,55 @@
+import { Eye, EyeClosed } from "lucide-react";
+import { Button } from "./ui/button";
+import { cn, shortenAddress } from "@/lib/utils";
+import { useUserNotes } from "@/hooks/useUserNotes";
+import { useMemo, useState } from "react";
 
 export const Notes: React.FC = () => {
+  const { notes } = useUserNotes();
+  const [show, setShow] = useState(false);
 
-    return (
-      <div className="flex flex-col p-6 bg-white rounded-3xl border border-primary">
-        <h1 className="mb-8 font-semibold">Notes</h1>
-  
+  const sortedNotes = useMemo(
+    () => notes.sort((a, b) => parseInt((b.index - a.index).toString())),
+    [notes]
+  );
+
+  return (
+    <div className="flex flex-col px-6 py-2 bg-white rounded-3xl border border-primary">
+      <div className="flex justify-between items-center py-2">
+        <h1 className="font-semibold">Notes</h1>
+        <Button
+          size="icon"
+          variant="ghost"
+          className="bg-white"
+          onClick={() => setShow(!show)}
+        >
+          {show ? <Eye /> : <EyeClosed />}
+        </Button>
       </div>
-    );
-  };
-  
-  
+
+      {sortedNotes && show && (
+        <ul className="divide-y border-t">
+          {sortedNotes.map((note) => (
+            <li className="flex justify-between py-6 pr-2">
+              <span
+                className={cn({
+                  "line-through": note.spent,
+                })}
+              >
+                {shortenAddress(note.commitment.toString(16))} (
+                {note.index.toString()})
+              </span>
+              <span
+                className={cn({
+                  "line-through": note.spent,
+                })}
+              >
+                {note.value?.toString()}
+              </span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
--- packages/ui/src/components/transfer.tsx
+++ packages/ui/src/components/transfer.tsx
@@ -2,12 +2,15 @@ import { useTransfer } from "@/hooks/useTransfer";
 import { useCallback, useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { QrCode } from "lucide-react";
+import { IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner";
 
 export const Transfer: React.FC = () => {
   const { sendTransfer, loading } = useTransfer();
   const [recipientAddress, setRecipientAddress] = useState("");
   const [recipientPublicKey, setRecipientPublicKey] = useState("");
   const [amount, setAmount] = useState(0);
+  const [scan, setScan] = useState(false);
 
   const onTransfer = useCallback(async () => {
     sendTransfer({
@@ -26,37 +29,57 @@ export const Transfer: React.FC = () => {
       });
   }, [amount, recipientAddress, recipientPublicKey, sendTransfer]);
 
+  const onScan = useCallback((result: IDetectedBarcode[]) => {
+    const data = JSON.parse(result[0].rawValue);
+
+    setRecipientAddress(data.address.startsWith("0x") ? data.address : `0x${data.address}`);
+    setRecipientPublicKey(data.publicKey.startsWith("0x") ? data.publicKey : `0x${data.publicKey}`);
+    setScan(false);
+  }, [setRecipientAddress, setRecipientPublicKey, setScan]);
+
   return (
     <div className="flex flex-col p-6 bg-white rounded-3xl border border-primary">
-      <h1 className="mb-8 font-semibold">Transfer</h1>
-
-      <div className="space-y-4 mb-12">
-        <Input
-          type="text"
-          placeholder="Recipient Address"
-          value={recipientAddress}
-          onChange={(e) => setRecipientAddress(e.target.value)}
-        />
-
-        <Input
-          type="text"
-          placeholder="Recipient Public Key"
-          value={recipientPublicKey}
-          onChange={(e) => setRecipientPublicKey(e.target.value)}
-        />
-
-        <Input
-          type="number"
-          placeholder="Amount"
-          value={amount}
-          onChange={(e) => setAmount(parseInt(e.target.value))}
-        />
+      <div className="flex items-center justify-between mb-6">
+        <h1 className="font-semibold">Transfer</h1>
+        <Button onClick={() => setScan(!scan)} size="icon">
+          <QrCode />
+        </Button>
       </div>
 
-      <Button className="w-full" onClick={onTransfer} disabled={loading}>
-        {loading ? "Transferring..." : "Transfer "}
-      </Button>
+      {scan ? (
+        <div>
+          <Scanner onScan={onScan} />
+        </div>
+      ) : (
+        <>
+          <div className="space-y-4 mb-12">
+            <Input
+              type="text"
+              placeholder="Recipient Address"
+              value={recipientAddress}
+              onChange={(e) => setRecipientAddress(e.target.value)}
+            />
+
+            <Input
+              type="text"
+              placeholder="Recipient Public Key"
+              value={recipientPublicKey}
+              onChange={(e) => setRecipientPublicKey(e.target.value)}
+            />
+
+            <Input
+              type="number"
+              placeholder="Amount"
+              value={amount}
+              onChange={(e) => setAmount(parseInt(e.target.value))}
+            />
+          </div>
+
+          <Button className="w-full" onClick={onTransfer} disabled={loading}>
+            {loading ? "Transferring..." : "Transfer "}
+          </Button>
+        </>
+      )}
     </div>
   );
 };
-
