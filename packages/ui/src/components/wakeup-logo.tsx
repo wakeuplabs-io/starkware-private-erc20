77927b671d4cffd4272333b8640aacba82ece97a
Matias Zapata <matuzapata@gmail.com>
2025-02-26 17:49:57 -0300

    fix build

--- packages/ui/src/components/wakeup-logo.tsx
+++ packages/ui/src/components/wakeup-logo.tsx
@@ -1,9 +1,9 @@
-export const WakeUpLogo: React.FC<{ className?: string, imageClassName?: string }> = ({ className, imageClassName }) => {
+export const WakeUpLogo: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <a href="https://wakeuplabs.io/" target="_blank">
       <img
         src="/wakeup-powered.png"
-        alt=""
+        alt="Wakeup logo"
         className={className}
       />
     </a>
